b36bad621044c8e422f34eaec8c417b6a671d14c
controlcoder <ritikaryan2210@gmail.com>
2024-09-22 23:12:31 +0530

    Splash Screen

--- frontend/RNapp/.expo/types/router.d.ts
+++ frontend/RNapp/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/../app-example(tabs)/_layout` | `/../app-example/+html` | `/../app-example/+not-found` | `/../app-example/_layout` | `/_sitemap` | `/splash`;
+      StaticRoutes: `/` | `/..\app-example\(tabs)\_layout` | `/..\app-example\+html` | `/..\app-example\+not-found` | `/..\app-example\_layout` | `/_sitemap` | `/splash` | `/splash\`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
