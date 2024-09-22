eda4955bec1eb5057a3f0946258c3416a2860b77
controlcoder <ritikaryan2210@gmail.com>
2024-09-22 23:11:52 +0530

    Test

--- frontend/RNapp/.expo/types/router.d.ts
+++ frontend/RNapp/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/_sitemap` | `/splash`;
+      StaticRoutes: `/(tabs)\_layout` | `/..\app-example\(tabs)\_layout` | `/_sitemap` | `/splash`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
