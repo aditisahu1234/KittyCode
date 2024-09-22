3a82272f6b7e0614977d89b6fd36b8aed074bf4b
controlcoder <ritikaryan2210@gmail.com>
2024-09-22 23:18:19 +0530

    changes

--- frontend/RNapp/.expo/types/router.d.ts
+++ frontend/RNapp/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/(tabs)\_layout` | `/..\app-example\(tabs)\_layout` | `/_sitemap` | `/splash`;
+      StaticRoutes: `/(tabs)\_layout` | `/_sitemap` | `/splash`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
